40e664a081d48d9238aa4a5abc3d6bb67b347921
Jonas Brito <jonasexplore@gmail.com>
2021-11-05 20:36:36 -0300

    feat: add enemy class on screen

--- __init__.py
+++ __init__.py
@@ -2,7 +2,9 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+from src.core.Enemy import Enemy
 from src.core.Player import Player
+
 from src.config.constants import *
 
 pygame.init()
@@ -18,12 +20,17 @@ sprites = pygame.sprite.Group()
 player = Player(sprite_dir=FROG_SPRITES_DIR, player_speed=PLAYER_SPEED)
 sprites.add(player)
 
+enemy = Enemy(sprite_dir=CAR_SPRITE_DIR)
+
 clock = pygame.time.Clock()
+
+background = pygame.image.load("./src/data/textures/background.jpg")
 
 while True:
     clock.tick(30)
 
     screen.fill(BLACK_COLOR)
+    screen.blit(background, (0, 0))
 
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -31,21 +38,22 @@ while True:
             pygame.quit()
             exit()
 
-        if keys[pygame.K_s]:
-            if player.animate == False:
+        if player.animate == False:
+            if keys[pygame.K_s]:
                 player.down()
-        if keys[pygame.K_w]:
-            if player.animate == False:
+            if keys[pygame.K_w]:
                 player.up()
-        if keys[pygame.K_a]:
-            if player.animate == False:
+            if keys[pygame.K_a]:
                 player.left()
-        if keys[pygame.K_d]:
-            if player.animate == False:
+            if keys[pygame.K_d]:
                 player.right()
 
     screen.blit(player.image, player.position())
+    screen.blit(enemy.image, enemy.position())
+
     sprites.draw(screen)
     sprites.update()
+
+    enemy.update()
 
     pygame.display.flip()
