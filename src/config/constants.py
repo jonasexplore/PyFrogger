4491db783a7c0d89a03f046927c05a2734903ee8
Jonas Brito <jonasexplore@gmail.com>
2021-11-20 15:02:01 -0300

    chore(config): add more configs

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,4 +1,8 @@
-FROG_SPRITES_DIR = './src/data/textures/frog/'
-CAR_SPRITE_DIR = './src/data/textures/car_v2.png'
+FROG_SPRITES_DIR = './src/resources/textures/frog/'
+CAR_SPRITE_DIR = './src/resources/textures/car_v2.png'
+FLY_SPRITE_DIR = './src/resources/textures/fly2.png'
+FROG_SPRITE_DIR = './src/resources/textures/frog/frog_v2.png'
 PLAYER_SPEED = 38
 BLACK_COLOR = (0, 0, 0)
+
+SPRITE_POS_RESTART = 150
