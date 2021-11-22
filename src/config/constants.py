fe195db830abe57a8a34448331dc1d89d5683741
Jonas Brito <jonasexplore@gmail.com>
2021-11-22 11:12:03 -0300

    chore: create a type for screen type

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 # Sprites
 FROG_SPRITES_DIR = './src/resources/textures/frog/'
 CAR_SPRITE_DIR = './src/resources/textures/car_v2.png'
@@ -12,4 +14,11 @@ FONT = 'arial'
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 MEDIUMSEAGREEN = (60, 179, 113)
-DARKGREEN = (0,100,0)
+DARKGREEN = (0, 100, 0)
+
+# Enums
+
+
+class screens:
+    MAIN_MENU = 1
+    START_GAME = 2
